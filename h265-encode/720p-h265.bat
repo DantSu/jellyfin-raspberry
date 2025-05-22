f1aa460ec91d595b9b503acca494d5392340dd54
Franck Alary <franck@alary.pro>
2025-05-22 09:16:39 +0200

    initial commit

--- h265-encode/720p-h265.bat
+++ h265-encode/720p-h265.bat
@@ -2,30 +2,8 @@
 chcp 65001
 setlocal enabledelayedexpansion
 
-SET paths[0]="Abby Hatcher, Fuzzly Catcher\Saison 01"
-SET paths[0]="Abby Hatcher, Fuzzly Catcher\Saison 02"
-SET paths[1]="Gabby's Dollhouse\Saison 01"
-SET paths[1]="Gabby's Dollhouse\Saison 02"
-SET paths[1]="Gabby's Dollhouse\Saison 03"
-SET paths[1]="Gabby's Dollhouse\Saison 04"
-SET paths[1]="Gabby's Dollhouse\Saison 05"
-SET paths[1]="Gabby's Dollhouse\Saison 06"
-SET paths[1]="Gabby's Dollhouse\Saison 07"
-SET paths[1]="Gabby's Dollhouse\Saison 08"
-SET paths[1]="Gabby's Dollhouse\Saison 09"
-SET paths[1]="Gabby's Dollhouse\Saison 10"
-SET paths[1]="Gabby's Dollhouse\Saison 11"
-SET paths[1]="My Little Pony - Friendship Is Magic\Saison 09"
-SET paths[1]="My Little Pony - Make Your Mark\Saison 01"
-SET paths[1]="My Little Pony - Make Your Mark\Saison 02"
-SET paths[1]="My Little Pony - Make Your Mark\Saison 03"
-SET paths[1]="My Little Pony - Make Your Mark\Saison 04"
-SET paths[1]="Princess Power\Saison 01"
-SET paths[1]="Princess Power\Saison 02"
-SET paths[1]="Sunny Bunnies\Saison 01"
-SET paths[1]="Unicorn Academy\Saison 01"
-SET paths[1]="Unicorn Academy\Saison 02"
-SET paths[1]="Unicorn Academy\Saison 03"
+SET paths[0]="Mes videos de famille"
+SET paths[1]="Mes videos professionnelles"
 
 SET pathPos=0
 
@@ -70,8 +48,8 @@ IF NOT EXIST "%pathDst%" mkdir "%pathDst%"
 
 ECHO "Transcoding..."
 
-"C:\Users\Franck\.telmi\bin\ffmpeg.exe" -y -i "%pathSrc%" -vf scale=1280:-1 -c:v libx265 -b:v 1500k -x265-params pass=1:no-slow-firstpass=1 -an -f null NUL && ^
-"C:\Users\Franck\.telmi\bin\ffmpeg.exe" -i "%pathSrc%" -vf scale=1280:-1 -c:v libx265 -b:v 1500k -x265-params pass=2 -c:a copy "%filePathDst%"
+"C:\Users\[MON_DOSSIER_UTILISATEUR]\.telmi\bin\ffmpeg.exe" -y -i "%pathSrc%" -vf scale=1280:-1 -c:v libx265 -b:v 1500k -x265-params pass=1:no-slow-firstpass=1 -an -f null NUL && ^
+"C:\Users\[MON_DOSSIER_UTILISATEUR]\.telmi\bin\ffmpeg.exe" -i "%pathSrc%" -vf scale=1280:-1 -c:v libx265 -b:v 1500k -x265-params pass=2 -c:a copy "%filePathDst%"
 
 IF EXIST "x265_2pass.log" DEL "x265_2pass.log"
 IF EXIST "x265_2pass.log.cutree" DEL "x265_2pass.log.cutree"
